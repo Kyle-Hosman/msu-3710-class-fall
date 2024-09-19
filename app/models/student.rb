d628808a3fa5a20857ec27dd30fc574ce26fbdf6
Kyle Hosman <khosman@msudenver.edu>
2024-09-19 16:28:22 -0600

    Update for Data Validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }
+    validates :school_email, presence: true, uniqueness: true
+    validates :major, presence: true
+    validates :minor, presence: true
+    validates :graduation_date, presence: true
+
     has_one_attached :profile_picture
 end

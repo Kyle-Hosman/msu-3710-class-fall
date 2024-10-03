7587e62f041051b1a0dbbe14bae0fc0fc77f0ab0
Kyle Hosman <khosman@msudenver.edu>
2024-10-03 12:22:23 -0600

    added major dropdown

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,10 @@ class Student < ApplicationRecord
     validates :graduation_date, presence: true
 
     has_one_attached :profile_picture
+
+    VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
+       "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+
+    validates :major, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major" }
+
 end
